6357bdf941bfd38aa1f377bd52fddc66df8453b2
Ashish-1605 <ashishjaiswal546789@gmail.com>
2024-07-30 21:28:02 +0530

    adding sum method in main class

--- Demo.java
+++ Demo.java
@@ -1,9 +1,24 @@
 package LocalRepo;
+import java.util.*;
 
-/**
- * Demo
- */
-public class Demo {
+
+ class Demo {
+    
+    void sum(){
+    int c;
+        System.out.println("Enter the value of a and b");
+        Scanner sc = new Scanner(System.in );
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    c=a+b;
+        System.out.println("Sum of  a and b =" +c );
+    }
+
+    public static void main(String[] args) {4
+        Demo d1 = new Demo();
+        d1.sum();
+        
+    }
 
     
 }
